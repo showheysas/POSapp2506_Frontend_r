582e176d92e23e201154a179925346eb26647c3a
showheysas <shoheysas@gmail.com>
2025-06-11 15:23:57 +0900

    next.config.js の env ブロックをコメントアウト

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
-  },
+  // env: {
+  //   NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
+  // },
 };
 
 module.exports = nextConfig;
